e7e7126159a59b093995cc1a5347291491d03a7c
khammami <khammami@gmail.com>
2024-05-04 18:06:42 +0100

    Clean up template

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  output: "export",
+}
+
+module.exports = withNextra(nextConfig)
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+//import 'nextra-theme-docs/style.css'
+
+function LgdApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default LgdApp
